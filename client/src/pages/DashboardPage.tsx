b715a6f8894930bef8a5d084646f0f4fd5355f35
Андрей <leo123messi123123@gmail.com>
2026-06-26 22:53:44 +0500

    Создана главная страница Академии

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -1,14 +1,69 @@
+import Button from "../components/common/Button/Button";
+import Card from "../components/common/Card/Card";
 import PageTitle from "../components/common/PageTitle/PageTitle";
+import Section from "../components/common/Section/Section";
+import "./DashboardPage.css";
 
 function DashboardPage() {
   return (
-    <div>
+    <div className="dashboard">
       <PageTitle
         title="Академия"
-        subtitle="Твой шахматный путь начинается здесь"
+        subtitle="Сегодня отличный день, чтобы стать сильнее в шахматах"
       />
 
-      <p>Главная панель ученика</p>
+      <div className="dashboard__grid">
+        <Card>
+          <Section title="План занятий">
+            <ul className="dashboard__task-list">
+              <li>Разминка: решить 3 лёгкие задачи</li>
+              <li>Практика: пройти тему «Мат в 1 ход»</li>
+              <li>Повторение: разобрать одну позицию</li>
+            </ul>
+          </Section>
+        </Card>
+
+        <Card>
+          <Section title="Миссия недели">
+            <strong>Тактический рывок</strong>
+            <p>Решить 25 задач по тактике</p>
+
+            <div className="dashboard__mission-progress">
+              <div className="dashboard__mission-progress-fill" />
+            </div>
+
+            <p>15 / 25 задач</p>
+          </Section>
+        </Card>
+      </div>
+
+      <Section title="Быстрые действия">
+        <div className="dashboard__actions">
+          <Card>
+            <div className="dashboard__action">
+              <h3>Практика</h3>
+              <p>Перейти к шахматным задачам.</p>
+              <Button>Начать</Button>
+            </div>
+          </Card>
+
+          <Card>
+            <div className="dashboard__action">
+              <h3>Учебный класс</h3>
+              <p>Позже здесь будут уроки и материалы тренера.</p>
+              <Button>Открыть</Button>
+            </div>
+          </Card>
+
+          <Card>
+            <div className="dashboard__action">
+              <h3>Профиль</h3>
+              <p>Посмотреть шахматный путь и достижения.</p>
+              <Button>Перейти</Button>
+            </div>
+          </Card>
+        </div>
+      </Section>
     </div>
   );
 }
